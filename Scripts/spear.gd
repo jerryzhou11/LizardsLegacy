a624676447edf224d52e7cecc2e7469124d3a6ca
StolenQuotient2 <31660179+StolenQuotient2@users.noreply.github.com>
2024-08-17 12:37:49 -0400

    Setting up Area2D catching

--- Scripts/spear.gd
+++ Scripts/spear.gd
@@ -10,7 +10,9 @@ const AIR_RESISTANCE = 0
 const VELOCITY_INHERETANCE = 0.5 #percent of player velocity that adds to throw
 
 @export var Character: NodePath
+@export var CatchZone: NodePath
 @onready var Collider := $CollisionShape2D
+
 
 enum SpearState {
 	CARRIED,
