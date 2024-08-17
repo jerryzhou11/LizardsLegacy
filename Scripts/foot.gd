d4d4a9fad7ce32a731057417b43be5266a6d9d8e
jerryzhou <122940736+kdfchse@users.noreply.github.com>
2024-08-16 22:08:59 -0500

    Progress on stomp
    The foot moves towards the player. The actual stomp is still WIP

--- Scripts/foot.gd
+++ Scripts/foot.gd
@@ -1,17 +1,19 @@
 extends CharacterBody2D
 var stomping : bool = true
-var acc : = 2000
-var target 
+var SPEED = 600
+@onready var player := get_parent().get_node("Player")
 
-@onready var collision_shape := $CollisionShape2D
+
+@onready var collision_shape := $Hitbox
 
 func _physics_process(delta: float) -> void:
 	if(stomping and not is_on_floor()):
 		stomp_move(delta)
 
 func stomp_move(delta)->void:
-	velocity.y += acc * delta
-	move_and_slide()
+	if player:
+		velocity = global_position.direction_to(player.global_position)
+		move_and_collide(velocity * SPEED * delta)
 
 
 func set_disabled(is_disabled:bool)->void:
