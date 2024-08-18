31377f50b9b651ca43c4f3357d37d34308eb004e
antigluon <optionaloctave@gmail.com>
2024-08-18 14:51:41 -0400

    flight ui shows only with wings

--- Scripts/flight_staminabar.gd
+++ Scripts/flight_staminabar.gd
@@ -5,10 +5,15 @@ var attached_character
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	attached_character = get_node(Character)
+	hide()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if attached_character:
-		value = attached_character.get_staminabar_percent()
+		if attached_character.items.wings:
+			show()
+			value = attached_character.get_staminabar_percent()
+		else:
+			hide()
