3468f4e301551ae4f8c4d54e33a178ba9e9ae51e
antigluon <optionaloctave@gmail.com>
2024-08-18 18:15:49 -0400

    fullscreen toggle

--- fullscreen_toggle.gd
+++ fullscreen_toggle.gd
@@ -3,8 +3,11 @@ extends CheckButton
 
 # Called when the node enters the scene tree for the first time.
 func _toggled(toggled_on: bool) -> void:
-	print("Fullscreen: ", toggled_on)
 	if toggled_on:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("toggle_fullscreen"):
+		button_pressed = not button_pressed
