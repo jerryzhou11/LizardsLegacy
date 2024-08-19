02bd831c7bb398120f76aa87ac71ba34447841d4
Giles Gordon <122418461+GilesGordon@users.noreply.github.com>
2024-08-19 01:06:12 -0400

    village, shop, bg, art imports

---
+++ Scripts/village.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$shop_ui.hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+# right side area
+func _on_next_stage_body_entered(body: Node2D) -> void:
+	if body.get_name() == "Player":
+		print("next stage entered")
+
+#enter shop
+func _on_shop_body_entered(body: Node2D) -> void:
+	if body.get_name() == "Player":
+		$shop_ui.show()
+	
+#exit shop	
+func _on_shop_body_exited(body: Node2D) -> void:
+	if body.get_name() == "Player":
+		$shop_ui.hide()
+		
