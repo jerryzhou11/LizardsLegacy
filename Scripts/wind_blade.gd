b4f388b575c94c467136f3df1d2ffe82e42d520e
jerryzhou <122940736+kdfchse@users.noreply.github.com>
2024-08-19 01:21:36 -0500

    wind blade tweaks
    im tweakin

--- Scripts/wind_blade.gd
+++ Scripts/wind_blade.gd
@@ -9,3 +9,8 @@ func _physics_process(delta: float) -> void:
 
 func _on_lifetime_timeout() -> void:
 	queue_free()
+
+
+func _on_body_entered(body:Node2D) -> void:
+	if body.is_in_group("dragon"):
+		queue_free()
