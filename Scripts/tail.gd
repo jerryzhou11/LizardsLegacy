f6fb499eccefd3c8c5c55c3ffa193bf31c268eda
Giles Gordon <122418461+GilesGordon@users.noreply.github.com>
2024-08-19 20:47:12 -0400

    tail animation

--- Scripts/tail.gd
+++ Scripts/tail.gd
@@ -1,24 +1,3 @@
-#extends CharacterBody2D
-#
-#@onready var collision_polygon = $CollisionPolygon2D
-#@onready var animation_player = $AnimationPlayer
-#@onready var tail_timer = $Tail_Cooldown
-#
-#func _process(delta: float) -> void:
-	#if is_not_on_cooldown():
-		#play_animation()
-		#start_tail_attack(7)
-	#
-#func start_tail_attack(dur: float) -> void:
-	#tail_timer.wait_time = dur
-	#tail_timer.start()
-#
-#func is_not_on_cooldown() -> bool:
-	#return tail_timer.is_stopped()
-	#
-#func play_animation():
-	#animation_player.play("tail_swiping")
-	
 extends CharacterBody2D
 
 @onready var collision_polygon = $CollisionPolygon2D
@@ -28,11 +7,14 @@ extends CharacterBody2D
 
 @export var cooldown : float = 2.5
 
-var can_attack = true
+func _ready():
+	tail_timer.timeout.connect(_on_Tail_Cooldown_timeout)
 
-func _process(delta: float) -> void:
+func _process(delta):
 	if can_attack:
-		start_tail_attack()
+		start_tail_attack()	
+	
+var can_attack = true
 
 func start_tail_attack() -> void:
 	can_attack = false
@@ -45,10 +27,6 @@ func _on_Tail_Cooldown_timeout():
 
 func play_animation():
 	animation_player.play("tail_swiping")
-	
-
-func _ready():
-	tail_timer.timeout.connect(_on_Tail_Cooldown_timeout)
 
 
 func _on_animation_player_animation_finished(anim_name:StringName) -> void:
