c14a41f95e6f4de0cb4c90b3601eff9bfff7f3d1
hwoodward26 <166756044+hwoodward26@users.noreply.github.com>
2024-08-19 23:47:41 -0400

    flames
    flames

--- Scripts/flames.gd
+++ Scripts/flames.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
 @export var speed: float = 400.0
+@onready var animated_sprite = $AnimatedSprite2D
 
 var direction: Vector2 = Vector2.ZERO
 
 func _physics_process(delta):
 	global_position += direction * speed * delta
+	animated_sprite.play("flicker")
 
 func initialize(start_pos: Vector2, target_pos: Vector2):
 	global_position = start_pos
